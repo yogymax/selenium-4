adb13dbe2383d5ef0000e7d1645c497a6734ba6c
Demo <demo>
2017-05-22 08:40:46 +0530

    first commit

---
+++ src/main/java/com/demo/gitdemo/App.java
@@ -0,0 +1,14 @@
+package com.demo.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("git....");
+    }
+}
